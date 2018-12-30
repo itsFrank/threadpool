54949155c9ba505a15e10c38c00584b81fc5d520
Francis O'Brien <francis.obrien@mail.utoronto.ca>
2018-12-30 14:44:32 -0500

    v 1.0.0

--- sample.cpp
+++ sample.cpp
@@ -1,5 +1,6 @@
 #include "threadpool.h"
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,19 +9,56 @@ typedef struct {
     double d;
 } job1_params_t;
 
+typedef struct {
+    vector<int>* a;
+    vector<int>* b;
+    vector<int>* c;
+    int start;
+    int count;
+} vsum_params_t;
 
 void job1(void* p) {
     job1_params_t jp = (job1_params_t) (*((job1_params_t*)p));
     cout << "From job 1: " << jp.i << " | " << jp.d << endl;
 }
 
+void vsum(void* p) {
+    vsum_params_t* vp = (vsum_params_t*) p;
+
+    for (int i = vp->start; i < vp->start + vp->count; i++) {
+        (*vp->c)[i] = vp->a->at(i) + vp->b->at(i);
+    }
+}
+
 int main() {
 
     ThreadPool pool(4);
 
-    job1_params_t jp = {12, 10.3};
-    pool.addJob(job1, &jp);
+    vector<int> a(20);
+    vector<int> b(20);
+    vector<int> c(20);
+
+    for (int i = 0; i < 20; i++) {
+        a[i] = i;
+        b[i] = 2*i;
+    }
+
+    vsum_params_t vp1 = {&a, &b, &c, 0, 4};
+    vsum_params_t vp2 = {&a, &b, &c, 5, 4};
+    vsum_params_t vp3 = {&a, &b, &c, 10, 4};
+    vsum_params_t vp4 = {&a, &b, &c, 15, 4};
+    
+    pool.addJob(vsum, &vp1);
+    pool.addJob(vsum, &vp2);
+    pool.addJob(vsum, &vp3);
+    pool.addJob(vsum, &vp4);
+
+    while(pool.jobsRemaining());
 
     pool.joinThreads();
 
+
+    for (int i = 0; i < 20; i++) {
+        cout << a[i] << " + " << b[i] << " = " << c[i] << endl;
+    }
 }
--- threadpool.h
+++ threadpool.h
@@ -1,19 +1,28 @@
 #include <queue>
 #include <vector>
+#include <iostream>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define JOB_CHECK_DELAY 100
 
 using namespace std;
 
+class ThreadPool;
+void* threadLoopHelper(void* args);
 typedef void job_funct_t(void*);
 
 typedef struct {
     void (*f)(void*);
     void* params;
 } job_t;
+
+typedef struct {
+    int tid;
+    ThreadPool* pool;
+} loop_helper_args_t;
 
 
 class ThreadPool
@@ -23,21 +32,22 @@ private:
     int thread_count;
     bool join_threads = false;
 
-    vector<int> join_ids;
-    vector<int> thread_ids;
     vector<pthread_t> threads;
 
     pthread_mutex_t queue_mutex;
 
     queue<job_t> job_queue;
 
+    struct timespec tim;
+   
 public:
     ThreadPool(int);
     ~ThreadPool();
     void addJob(job_funct_t, void*);
-    void fetchJob();
+    void fetchJob(int);
+    bool jobsRemaining();
     void joinThreads();
-    void* threadLoop(void*);
+    void threadLoop(int);
 };
 
 ThreadPool::ThreadPool(int thread_count)
@@ -45,12 +55,18 @@ ThreadPool::ThreadPool(int thread_count)
     this->thread_count = thread_count;
     this->queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+    this->tim.tv_sec = 0;
+    this->tim.tv_nsec = JOB_CHECK_DELAY;
+
     this->threads.resize(thread_count);
+    this->job_queue = queue<job_t>();
     
     for (int i = 0; i < thread_count; i++) {
-        this->thread_ids.push_back(i);
-        int jid = pthread_create(&this->threads[i], NULL, threadLoop, &this->thread_ids[i]);
-        this->join_ids.push_back(jid);
+        loop_helper_args_t* lh_args = new loop_helper_args_t;
+        lh_args->tid = i;
+        lh_args->pool = this;
+
+        pthread_create(&this->threads[i], NULL, threadLoopHelper, lh_args);
     }
 }
 
@@ -66,40 +82,58 @@ void ThreadPool::addJob(void (*f)(void*), void* params) {
     pthread_mutex_unlock(&this->queue_mutex);
 }
 
-void ThreadPool::fetchJob() {
-    bool job_fetched = false;
+void ThreadPool::fetchJob(int tid) {
 
     pthread_mutex_lock(&this->queue_mutex);
     
-    if (this->job_queue.size > 0) {
-        job_t job = this->job_queue.front();
+    job_t job;
+    bool job_fetched = false;
+    
+    if (!this->job_queue.empty()) {
+        job = this->job_queue.front();
         this->job_queue.pop();
         job_fetched = true;
     }
 
     pthread_mutex_unlock(&this->queue_mutex);
 
-    if (job_fetched) job.f(job.params);
+    if (job_fetched) {
+        job.f(job.params);
+    }
 }
 
-void ThreadPool::joinThreads(void* args) {
+bool ThreadPool::jobsRemaining() {
+
+    pthread_mutex_lock(&this->queue_mutex);
+    
+    bool remaining = !this->job_queue.empty();
+
+    pthread_mutex_unlock(&this->queue_mutex);
+
+    return remaining;
+}
+
+void ThreadPool::joinThreads() {
     this->join_threads = true;
 
-    // For join...
+    for (int i = 0; i < this->thread_count; i++) {
+        pthread_join(this->threads[i], NULL);
+    }
 }
 
-void* ThreadPool::threadLoop(void* args) {
-    int tid = *((int*)args);
-    
-    cout << "Thread " << tid << " started" << endl;
+void ThreadPool::threadLoop(int tid) {
     while (!this->join_threads) {
-        this->fetchJob();
-        // Wait delay
+        this->fetchJob(tid);
+        nanosleep(&this->tim, NULL);
     }
-    
-    cout << "Thread " << tid << " stopped" << endl;
 }
 
 ThreadPool::~ThreadPool()
 {
+}
+
+void* threadLoopHelper(void* args) {
+    loop_helper_args_t* lh_args = (loop_helper_args_t*) args;
+
+    lh_args->pool->threadLoop(lh_args->tid);
 }
